8a8d398d680c7cacc5a31381da4faad748f89bec
jsmolina <jordi.sesmeromolina@telefonica.com>
2026-02-26 19:04:21 +0100

    feat: add midi

--- src/statics.h
+++ src/statics.h
@@ -1,6 +1,6 @@
 /* Allegro datafile object indexes, produced by dat v4.4.3, Unix */
 /* Datafile: datos.dat */
-/* Date: Thu Feb 26 12:44:28 2026 */
+/* Date: Thu Feb 26 17:37:51 2026 */
 /* Do not hand edit! */
 
 #define BG0_TMX                          0        /* DATA */
@@ -9,9 +9,10 @@
 #define COCHE_SPRITESHEET_BMP            3        /* BMP  */
 #define JOVEN_SPRITESHEET_BMP            4        /* BMP  */
 #define LEVEL2_INTRO_BMP                 5        /* BMP  */
-#define MARTIN_SPRITESHEET_BMP           6        /* BMP  */
-#define MENU2_BMP                        7        /* BMP  */
-#define PALETE_INTRO_BMP                 8        /* PAL  */
-#define PIELDETORO_INTRO3_BMP            9        /* BMP  */
-#define TILES_BMP                        10       /* BMP  */
+#define MANHATTAN_PROJECT_MID            6        /* MIDI */
+#define MARTIN_SPRITESHEET_BMP           7        /* BMP  */
+#define MENU2_BMP                        8        /* BMP  */
+#define PALETE_INTRO_BMP                 9        /* PAL  */
+#define PIELDETORO_INTRO3_BMP            10       /* BMP  */
+#define TILES_BMP                        11       /* BMP  */
 
