4868841cee3a47758346a25f5c088472aabdccdb
jsmolina <jordi.sesmeromolina@telefonica.com>
2026-03-23 18:57:47 +0100

    feat: implement lifebar

--- static/statics.h
+++ static/statics.h
@@ -1,6 +1,6 @@
 /* Allegro datafile object indexes, produced by dat v4.4.3, Unix */
 /* Datafile: datos.dat */
-/* Date: Fri Mar 20 14:59:23 2026 */
+/* Date: Mon Mar 23 17:57:03 2026 */
 /* Do not hand edit! */
 
 #define BG0_TMX                          0        /* DATA */
@@ -9,17 +9,21 @@
 #define BOOK_BMP                         3        /* BMP  */
 #define COCHE_SPRITESHEET_BMP            4        /* BMP  */
 #define DOG_SPRITESHEET_BMP              5        /* BMP  */
-#define INTRO_MID                        6        /* MIDI */
-#define JORDI_LOGO_BMP                   7        /* BMP  */
-#define JOVEN_SPRITESHEET_BMP            8        /* BMP  */
-#define LEVEL2_INTRO_BMP                 9        /* BMP  */
-#define MARTIN_SPRITESHEET_BMP           10       /* BMP  */
-#define MENU2_BMP                        11       /* BMP  */
-#define MSDOS_MID                        12       /* MIDI */
-#define MSDOSCLUB_BMP                    13       /* BMP  */
-#define PALETE_INTRO_BMP                 14       /* PAL  */
-#define PALETE_JORDI_LOGO_BMP            15       /* PAL  */
-#define PIELDETORO_INTRO3_BMP            16       /* BMP  */
-#define STAGE_ENEMIES_DEF                17       /* DATA */
-#define TILES_BMP                        18       /* BMP  */
+#define HEAD_BMP                         6        /* BMP  */
+#define INTRO_MID                        7        /* MIDI */
+#define JORDI_LOGO_BMP                   8        /* BMP  */
+#define JOVEN_SPRITESHEET_BMP            9        /* BMP  */
+#define LEVEL2_INTRO_BMP                 10       /* BMP  */
+#define LIFEBAR_BMP                      11       /* BMP  */
+#define LIFEBAR_BRUNO_BMP                12       /* BMP  */
+#define LIFEBAR_MARTIN_BMP               13       /* BMP  */
+#define MARTIN_SPRITESHEET_BMP           14       /* BMP  */
+#define MENU2_BMP                        15       /* BMP  */
+#define MSDOS_MID                        16       /* MIDI */
+#define MSDOSCLUB_BMP                    17       /* BMP  */
+#define PALETE_INTRO_BMP                 18       /* PAL  */
+#define PALETE_JORDI_LOGO_BMP            19       /* PAL  */
+#define PIELDETORO_INTRO3_BMP            20       /* BMP  */
+#define STAGE_ENEMIES_DEF                21       /* DATA */
+#define TILES_BMP                        22       /* BMP  */
 
