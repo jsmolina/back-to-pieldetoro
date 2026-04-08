30a3de30e62c4e16840db95d5f2a809162ef57de
jsmolina <jordi.sesmeromolina@telefonica.com>
2026-04-08 19:52:11 +0200

    update coin sprite

--- src/statics.h
+++ src/statics.h
@@ -1,6 +1,6 @@
 /* Allegro datafile object indexes, produced by dat v4.4.3, Unix */
 /* Datafile: datos.dat */
-/* Date: Wed Apr  8 16:53:40 2026 */
+/* Date: Wed Apr  8 17:51:21 2026 */
 /* Do not hand edit! */
 
 #define BG0_TMX                          0        /* DATA */
