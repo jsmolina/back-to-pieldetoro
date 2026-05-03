3428fba31496b6f08623c3e3c6c951ff0b5e838e
jsmolina <jordi.sesmeromolina@telefonica.com>
2026-05-03 23:30:04 +0200

    feat: improve first level

--- src/statics.h
+++ src/statics.h
@@ -1,6 +1,6 @@
 /* Allegro datafile object indexes, produced by dat v4.4.3, Unix */
 /* Datafile: datos.dat */
-/* Date: Mon Apr 27 14:22:58 2026 */
+/* Date: Sun May  3 21:25:36 2026 */
 /* Do not hand edit! */
 
 #define BG0_TMX                          0        /* DATA */
@@ -8,29 +8,30 @@
 #define BG1_SHOP_TMX                     2        /* DATA */
 #define BG1_TMX                          3        /* DATA */
 #define BG2_TMX                          4        /* DATA */
-#define BIRD_SPRITESHEET_BMP             5        /* BMP  */
-#define BOOK_BMP                         6        /* BMP  */
-#define COCHE_SPRITESHEET_BMP            7        /* BMP  */
-#define DOG_SPRITESHEET_BMP              8        /* BMP  */
-#define FONT_FNT                         9        /* FONT */
-#define HEAD_BMP                         10       /* BMP  */
-#define INTRO2_FLI                       11       /* FLIC */
-#define INTRO_FLI                        12       /* FLIC */
-#define INTRO_MID                        13       /* MIDI */
-#define JORDI_LOGO_BMP                   14       /* BMP  */
-#define JOVEN_SPRITESHEET_BMP            15       /* BMP  */
-#define LEVEL2_INTRO_BMP                 16       /* BMP  */
-#define LIFEBAR_BMP                      17       /* BMP  */
-#define LIFEBAR_BRUNO_BMP                18       /* BMP  */
-#define LIFEBAR_MARTIN_BMP               19       /* BMP  */
-#define MARTIN_SPRITESHEET_BMP           20       /* BMP  */
-#define MENU2_BMP                        21       /* BMP  */
-#define MONEY_BMP                        22       /* BMP  */
-#define MSDOS_MID                        23       /* MIDI */
-#define MSDOSCLUB_BMP                    24       /* BMP  */
-#define NUMBERS_BMP                      25       /* BMP  */
-#define PALETE_INTRO_BMP                 26       /* PAL  */
-#define PALETE_JORDI_LOGO_BMP            27       /* PAL  */
-#define PIELDETORO_INTRO3_BMP            28       /* BMP  */
-#define TILES_BMP                        29       /* BMP  */
+#define BG3_TMX                          5        /* DATA */
+#define BIRD_SPRITESHEET_BMP             6        /* BMP  */
+#define BOOK_BMP                         7        /* BMP  */
+#define COCHE_SPRITESHEET_BMP            8        /* BMP  */
+#define DOG_SPRITESHEET_BMP              9        /* BMP  */
+#define FONT_FNT                         10       /* FONT */
+#define HEAD_BMP                         11       /* BMP  */
+#define INTRO2_FLI                       12       /* FLIC */
+#define INTRO_FLI                        13       /* FLIC */
+#define INTRO_MID                        14       /* MIDI */
+#define JORDI_LOGO_BMP                   15       /* BMP  */
+#define JOVEN_SPRITESHEET_BMP            16       /* BMP  */
+#define LEVEL2_INTRO_BMP                 17       /* BMP  */
+#define LIFEBAR_BMP                      18       /* BMP  */
+#define LIFEBAR_BRUNO_BMP                19       /* BMP  */
+#define LIFEBAR_MARTIN_BMP               20       /* BMP  */
+#define MARTIN_SPRITESHEET_BMP           21       /* BMP  */
+#define MENU2_BMP                        22       /* BMP  */
+#define MONEY_BMP                        23       /* BMP  */
+#define MSDOS_MID                        24       /* MIDI */
+#define MSDOSCLUB_BMP                    25       /* BMP  */
+#define NUMBERS_BMP                      26       /* BMP  */
+#define PALETE_INTRO_BMP                 27       /* PAL  */
+#define PALETE_JORDI_LOGO_BMP            28       /* PAL  */
+#define PIELDETORO_INTRO3_BMP            29       /* BMP  */
+#define TILES_BMP                        30       /* BMP  */
 
