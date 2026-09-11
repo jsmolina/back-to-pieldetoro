3ca3b443d681f4cf3d68ebaaa319d0df4125f930
jsmolina <jordi.sesmeromolina@telefonica.com>
2026-09-12 00:43:22 +0200

    chore: test github actions windows

--- static/statics.h
+++ static/statics.h
@@ -1,6 +1,6 @@
 /* Allegro datafile object indexes, produced by dat v4.4.3, Unix */
 /* Datafile: datos.dat */
-/* Date: Fri Sep 11 21:29:10 2026 */
+/* Date: Fri Sep 11 21:32:21 2026 */
 /* Do not hand edit! */
 
 #define AAA_WAV                          0        /* SAMP */
