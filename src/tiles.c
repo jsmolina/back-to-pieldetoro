f795a2b219070fc43ed50eb36c46be0383852bd7
jsmolina <jordi.sesmeromolina@telefonica.com>
2026-09-14 08:14:24 +0200

    fix: tmx loading using size

--- src/tiles.c
+++ src/tiles.c
@@ -49,10 +49,19 @@ inline void load_tiles() {
 
 
 BITMAP* load_shop_bg(int id) {
-    char* in_file = dat_file[id].dat;
-    if (in_file == NULL) {
+    char* in_file;
+    if (dat_file[id].dat == NULL) {
         die("cannot load shop background %d", id);
     }
+    if (dat_file[id].size <= 0) {
+        die("empty shop background %d", id);
+    }
+    in_file = malloc((size_t)dat_file[id].size + 1);
+    if (in_file == NULL) {
+        die("cannot allocate shop background %d", id);
+    }
+    memcpy(in_file, dat_file[id].dat, (size_t)dat_file[id].size);
+    in_file[dat_file[id].size] = '\0';
 
     unsigned char current;
     int start_csv = 0;
@@ -128,14 +137,24 @@ BITMAP* load_shop_bg(int id) {
         }
     } while (current != '\0' && start_csv != -1);
 
+    free(in_file);
     return background;
 }
 
 BITMAP* load_background(int id) {
-    char* in_file = dat_file[id].dat;
-    if (in_file == NULL) {
+    char* in_file;
+    if (dat_file[id].dat == NULL) {
         die("cannot load %s", id);
     }
+    if (dat_file[id].size <= 0) {
+        die("empty background %d", id);
+    }
+    in_file = malloc((size_t)dat_file[id].size + 1);
+    if (in_file == NULL) {
+        die("cannot allocate background %d", id);
+    }
+    memcpy(in_file, dat_file[id].dat, (size_t)dat_file[id].size);
+    in_file[dat_file[id].size] = '\0';
     // BITMAP * background = create_bitmap(screen_w, SCREEN_H);
     // rectfill(background, 0, 0, SCREEN_W, SCREEN_H, makecol(16, 16, 16));
     unsigned char current;
@@ -183,7 +202,7 @@ BITMAP* load_background(int id) {
         die("invalid tiles width: %d", curr_tiles_width);
     }
 
-    map_width = (curr_tiles_width << 3) - SCREEN_W;
+    map_width = (curr_tiles_width << 3) - 320;
     map_pixel_width = curr_tiles_width << 3;
     if (map_pixel_width <= 0) {
         die("invalid map pixel width: %d", map_pixel_width);
@@ -244,6 +263,7 @@ BITMAP* load_background(int id) {
         // if (iterations == 200) start_csv = -1; // Uncomment if needed
     } while (current != '\0' && start_csv != -1);
 
+    free(in_file);
     return background;
 }
 /*
--- src/tnt.c
+++ src/tnt.c
@@ -70,6 +70,7 @@ void reset_tnt() {
 
 void load_level_tnt(int level_id) {
     int tmx_id = level_to_dat_id(level_id);
+    char* tmx_text;
     const char* cursor;
     int tidx = 0;
     int bidx = 0;
@@ -81,8 +82,17 @@ void load_level_tnt(int level_id) {
     if (dat_file[tmx_id].dat == NULL) {
         die("cannot load TMX data for level %d (tnt)", level_id);
     }
+    if (dat_file[tmx_id].size <= 0) {
+        die("empty TMX data for level %d (tnt)", level_id);
+    }
 
-    cursor = (const char*)dat_file[tmx_id].dat;
+    tmx_text = malloc((size_t)dat_file[tmx_id].size + 1);
+    if (tmx_text == NULL) {
+        die("cannot allocate TMX data for level %d (tnt)", level_id);
+    }
+    memcpy(tmx_text, dat_file[tmx_id].dat, (size_t)dat_file[tmx_id].size);
+    tmx_text[dat_file[tmx_id].size] = '\0';
+    cursor = tmx_text;
 
     while ((cursor = strstr(cursor, "<object ")) != NULL) {
         int id;
@@ -95,7 +105,7 @@ void load_level_tnt(int level_id) {
 
         if (matched >= 5 && strcmp(name, "TNT") == 0 && tidx < MAX_TNT) {
             tnts[tidx].x = x;
-            tnts[tidx].y = y-14;
+            tnts[tidx].y = y - 14;
             tnts[tidx].active = TRUE;
             tnts[tidx].collected = FALSE;
             tidx++;
@@ -111,6 +121,7 @@ void load_level_tnt(int level_id) {
 
         cursor++; // advance past current '<' to find next tag
     }
+    free(tmx_text);
     tnt_count = tidx;
     box_count = bidx;
 }
